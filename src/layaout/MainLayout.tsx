366feaa73936d8289b39eae407c0f96e84cb1d6b
JeanPierreAlemanD <pierre.aleleman.13@mgail.com>
2025-05-28 22:40:01 -0500

    correcion

--- src/layaout/MainLayout.tsx
+++ src/layaout/MainLayout.tsx
@@ -1,6 +1,5 @@
 import ProductList from "@pages/ProductsList/productList";
 import HomePage from "@pages/Home/HomePage ";
-import { Category } from "@models/product";
 
 console.log("ℹ️ MainLayout cargado (lazy)");
 
@@ -9,9 +8,6 @@ const MainLayout = () => {
         <div>
             <HomePage></HomePage>
             <ProductList title="New Arrivals" />
-            <ProductList category={Category.Electronics} title="Electronics" />
-            <ProductList category={Category.Jewelery} title="Jewelery" />
-            <ProductList category={Category.WomensClothing} title="Woman" />
         </div>
     );
 };
--- src/models/product.ts
+++ src/models/product.ts
@@ -8,19 +8,19 @@ export interface IResProducts {
   rating: Rating;
 }
 
-export enum Category {
-  MensClothing = "men's clothing",
-  WomensClothing = "women's clothing",
-  Jewelery = "jewelery",
-  Electronics = "electronics"
-}
-
-// export interface Category {
-//   id: number;
-//   name: string;
-//   image: string;
-//   slug: string;
+// export enum Category {
+//   MensClothing = "men's clothing",
+//   WomensClothing = "women's clothing",
+//   Jewelery = "jewelery",
+//   Electronics = "electronics"
 // }
+
+export interface Category {
+  id: number;
+  name: string;
+  image: string;
+  slug: string;
+}
 
 export interface Rating {
   rate: number;
